1abbb66b1e452e43465ff1861cb2a74543bf889f
josh <jhell85@gmail.com>
2020-03-30 15:35:36 -0700

    practiced mapping in node.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {
-  let myArray = [];
-  //on click function
-  $("form").submit(function(event) {
-    event.preventDefault();
-    let input1 = $("#input-1").val();
-    let input2 = $("#input-2").val();
-    let input3 = $("#input-3").val();
-    let input4 = $("#input-4").val();
 
-    myArray.push(input1, input2, input3);
-    myArray.unshift(input4)
 
-    $(".output ul").append("<li>-------------click--------------</li>");
-    myArray.forEach(function(input) {
-      $(".output ul").append(`<li>${input}</li>`);
-    });
+// var numbers = [1, 2, 3, 4, 5];
 
-    
-    myArray = [];
-  });
-});
+// var doubleNumbers = numbers.map(function(number) {
+//   number *= 2;
+//   console.log(number);
+//   return number;
+// });
+
+// $(document).ready(function() {
+//   let myArray = [];
+//   //on click function
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     let input1 = $("#input-1").val();
+//     let input2 = $("#input-2").val();
+//     let input3 = $("#input-3").val();
+//     let input4 = $("#input-4").val();
+
+//     myArray.push(input1, input2, input3);
+//     myArray.unshift(input4)
+
+//     $(".output ul").append("<li>-------------click--------------</li>");
+//     myArray.forEach(function(input) {
+//       $(".output ul").append(`<li>${input}</li>`);
+//     });
+
+//     myArray = [];
+//   });
+// });
 
 // $(document).ready(function() {
 //   $("#blanks form").submit(function(event) {
@@ -32,21 +41,21 @@ $(document).ready(function() {
 //     });
 
 //     $("story").show();
-//     // var person1Input = $("input#person1").val()
-//     // var person2Input = $("input#person2").val()
-//     // var animalInput= $("input#animal").val()
-//     // var exclamationInput = $("input#exclamation").val()
-//     // var verbInput = $("input#verb").val()
-//     // var nounInput = $("input#noun").val()
+//     var person1Input = $("input#person1").val()
+//     var person2Input = $("input#person2").val()
+//     var animalInput= $("input#animal").val()
+//     var exclamationInput = $("input#exclamation").val()
+//     var verbInput = $("input#verb").val()
+//     var nounInput = $("input#noun").val()
 
-//     // $(".person1").text(person1Input);
-//     // $(".person2").text(person2Input);
-//     // $(".animal").text(animalInput);
-//     // $(".exclamation").text(exclamationInput);
-//     // $(".verb").text(verbInput);
-//     // $(".noun").text(nounInput);
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
 
-//     // $("#story").show();
+//     $("#story").show();
 
 //   });
 // });
